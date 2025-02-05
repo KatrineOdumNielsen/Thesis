cdf1b254c0bf7e3e49434ed05cf9f6f1a2557515
Katrine Odum Nielsen <kani20af@student.cbs.dk>
2025-02-05 18:44:48 +0100

    Testing whether connection works to origin

--- py_code/test.py
+++ py_code/test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the CSV file
-bond_return_data = pd.read_csv("thesis/data/raw/bond_returns.csv")
+bond_return_data = pd.read_csv("/Users/mac/Library/CloudStorage/OneDrive-CBS-CopenhagenBusinessSchool/Python/Thesis/data/raw/bond_returns.csv")
 
 # View the first 5 rows
 bond_return_data()
