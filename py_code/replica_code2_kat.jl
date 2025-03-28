f0a0f5b8c0e4505f6e3a99281fd7b941282355a5
Katrine Odum Nielsen <kani20af@student.cbs.dk>
2025-03-28 15:28:41 +0100

    ITS WORKING

--- py_code/replica_code2_kat.jl
+++ py_code/replica_code2_kat.jl
@@ -122,7 +122,7 @@ theta_i_minus1_all = theta_all.theta_i_minus1
 θ̂ᵢ = zeros(3,1)
 
 for j = 1:3
-    println("I am calculating μ̂ and θ̂ᵢ for momentum decile ",j)
+    println("I am calculating μ̂ and θ̂ᵢ for portfolio ",j)
 
     σᵢ = σᵢ_all[j]
     βᵢ = βᵢ_all[j]
@@ -133,13 +133,33 @@ for j = 1:3
     theta_i_minus1 = theta_i_minus1_all[j]
 
     # Define function p_Ri
+    # function p_Ri(Ri, mu, Si, xi)
+    #     N = 1
+    #     Kl = besselk((nu + N) / 2, sqrt((nu + ((Ri - mu) ^ 2)/Si) * (xi^2) /Si))
+
+    #     result = (2^(1-(nu+N)/2)) / ( gamma(nu/2) * ((pi * nu)^(N/2)) * (abs(Si)^(1/2))) * (Kl * exp( (Ri - mu) / Si * xi )) / ( (sqrt((nu+((Ri - mu)^2) /Si) * (xi^2) /Si) )^(-(nu+N)/2) * (1+(Ri - mu)^2 / (Si * nu)) ^((nu+N)/2) )
+
+    #     return result
+    # end
+
     function p_Ri(Ri, mu, Si, xi)
-        N = 1
-        Kl = besselk((nu + N) / 2, sqrt((nu + ((Ri - mu) ^ 2)/Si) * (xi^2) /Si))
-
-        result = (2^(1-(nu+N)/2)) / ( gamma(nu/2) * ((pi * nu)^(N/2)) * (abs(Si)^(1/2))) * (Kl * exp( (Ri - mu) / Si * xi )) / ( (sqrt((nu+((Ri - mu)^2) /Si) * (xi^2) /Si) )^(-(nu+N)/2) * (1+(Ri - mu)^2 / (Si * nu)) ^((nu+N)/2) )
-
-        return result
+        threshold = 0.1
+    
+        if abs(xi) < threshold
+            # Formula for the case ξ = 0
+            # p(Ri) = Γ((ν+1)/2) / [ Γ(ν/2)* √(π·ν·Si ) ] * [1 + (Ri-μ)² / (ν·Si)]^(-(ν+1)/2)
+            return gamma((nu + 1) / 2) / ( gamma(nu / 2) * sqrt(pi * nu * Si) ) *
+                   ((1 + ((Ri - mu)^2)*nu^(-1)) / Si)^(-(nu + 1) / 2)
+        else
+            # Formula for the case ξ ≠ 0
+            N = 1
+            Kl = besselk((nu + N) / 2, sqrt((nu + ((Ri - mu)^2) / Si) * (xi^2) / Si))
+    
+            return (2^(1 - (nu + N) / 2)) / ( gamma(nu / 2) * ((pi * nu)^(N / 2)) * sqrt(abs(Si)) ) *
+                   ( Kl * exp( (Ri - mu) / Si * xi ) ) /
+                   ( (sqrt((nu + ((Ri - mu)^2) / Si) * (xi^2) / Si))^(-(nu + N) / 2) *
+                     (1 + (Ri - mu)^2 / (Si * nu))^((nu + N) / 2) )
+        end
     end
 
     # Define P_Ri
@@ -170,10 +190,28 @@ for j = 1:3
 
     # Define neg_integral
     function neg_integral(mu, Si, xi, g_i, theta_mi,theta_i_minus1)
-        integral, err = quadgk(x -> ((theta_mi * (Rf-x) - theta_i_minus1 * g_i) ^(α-1))* (Rf-x) * dwP_Ri(x, mu, Si, xi), -100, Rf-theta_i_minus1*g_i/theta_mi, rtol=1e-8)
+        integral, err = quadgk(x -> ((theta_mi * (Rf-x) - theta_i_minus1 * g_i) ^(α-1))* (Rf-x) * dwP_Ri(x, mu, Si, xi), 
+        -100, Rf-theta_i_minus1*g_i/theta_mi, rtol=1e-10)
 
         return integral
     end
+
+    # function pos_integral(mu, Si, xi, g_i, theta_mi, theta_i_minus1)
+    #     # Define integration limits:
+    #     a = Rf - theta_i_minus1 * g_i / theta_mi  # e.g. a = 0.147
+    #     b = 100.0
+    #     δ_split = 1e-8   # a small number to exclude x=1
+    
+    #     # Define the integrand function.
+    #     integrand(x) = ((theta_mi * (x - Rf) + theta_i_minus1 * g_i)^(α - 1)) *
+    #                    (x - Rf) *
+    #                    dwP_1_Ri(x, mu, Si, xi)
+    
+    #     # Split the integration domain to avoid x = 1.
+    #     int1, err1 = quadgk(integrand, a, 1.0 - δ_split, rtol=1e-10)
+    #     int2, err2 = quadgk(integrand, 1.0 + δ_split, b, rtol=1e-10)
+    #     return int1 + int2
+    # end
 
     # Define pos_integral
     function pos_integral(mu, Si, xi, g_i, theta_mi,theta_i_minus1)
@@ -182,13 +220,12 @@ for j = 1:3
         return integral
     end
 
-
     # Define neg_integral in Equation 20
     function neg_integral20(θᵢ, mu, Si, xi, g_i,theta_i_minus1,lamb, b0)
         if θᵢ >= 0
-            integral, err = quadgk(x -> (-lamb * b0 *(θᵢ * (Rf-x) - theta_i_minus1 * g_i ) ^(α)) * dwP_Ri(x, mu, Si, xi), -100, Rf-theta_i_minus1*g_i/θᵢ, rtol=1e-8)
+            integral, err = quadgk(x -> (-lamb * b0 *(θᵢ * (Rf-x) - theta_i_minus1 * g_i ) ^(α)) * dwP_Ri(x, mu, Si, xi), -100, Rf-theta_i_minus1*g_i/θᵢ, rtol=1e-10)
         elseif θᵢ < 0
-            integral, err = quadgk(x -> (b0 *(θᵢ * (x-Rf) + theta_i_minus1 * g_i) ^(α)) * dwP_Ri(x, mu, Si, xi), -100, Rf-theta_i_minus1*g_i/θᵢ, rtol=1e-8)
+            integral, err = quadgk(x -> (b0 *(θᵢ * (x-Rf) + theta_i_minus1 * g_i) ^(α)) * dwP_Ri(x, mu, Si, xi), -100, Rf-theta_i_minus1*g_i/θᵢ, rtol=1e-10)
         end
 
         return integral
@@ -197,9 +234,9 @@ for j = 1:3
     # Define pos_integral in Equation 20
     function pos_integral20(θᵢ, mu, Si, xi, g_i,theta_i_minus1,lamb, b0)
         if θᵢ >= 0
-            integral, err = quadgk(x -> (-b0 * (θᵢ * (x-Rf) + theta_i_minus1 * g_i) ^(α)) * dwP_1_Ri(x, mu, Si, xi), Rf-theta_i_minus1*g_i/θᵢ, 100, rtol=1e-8)
+            integral, err = quadgk(x -> (-b0 * (θᵢ * (x-Rf) + theta_i_minus1 * g_i) ^(α)) * dwP_1_Ri(x, mu, Si, xi), Rf-theta_i_minus1*g_i/θᵢ, 100, rtol=1e-10)
         elseif θᵢ < 0
-            integral, err = quadgk(x -> (lamb * b0 * (θᵢ * (Rf-x) - theta_i_minus1 * g_i ) ^(α)) * dwP_1_Ri(x, mu, Si, xi), Rf-theta_i_minus1*g_i/θᵢ, 100, rtol=1e-8)
+            integral, err = quadgk(x -> (lamb * b0 * (θᵢ * (Rf-x) - theta_i_minus1 * g_i ) ^(α)) * dwP_1_Ri(x, mu, Si, xi), Rf-theta_i_minus1*g_i/θᵢ, 100, rtol=1e-10)
         end
 
         return integral
@@ -208,9 +245,9 @@ for j = 1:3
 
     # Solve Equation 35 and get μ̂
     function Equation35(mu)
-        term1 = (mu[j] + (nu * xi / (nu-2) - Rf)) - γ̂ * βᵢ * σm ^ 2
-        term2 = -α * lamb * b0 * neg_integral(mu[j], Si, xi, g_i,theta_mi,theta_i_minus1)
-        term3 = - α * b0 * pos_integral(mu[j], Si, xi, g_i,theta_mi,theta_i_minus1)
+        term1 = (mu[1] + (nu * xi / (nu-2) - Rf)) - γ̂ * βᵢ * σm ^ 2
+        term2 = -α * lamb * b0 * neg_integral(mu[1], Si, xi, g_i,theta_mi,theta_i_minus1)
+        term3 = - α * b0 * pos_integral(mu[1], Si, xi, g_i,theta_mi,theta_i_minus1)
 
         return term1 + term2 + term3
     end
@@ -219,9 +256,9 @@ for j = 1:3
     # Equation 20
     function Equation20(θᵢ,μ̂)
 
-        term1 = θᵢ[j] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[j]^2 * σᵢ^2 + 2*θᵢ[j]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
-        term2 =  neg_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
-        term3 =  pos_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+        term1 = θᵢ[1] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[1]^2 * σᵢ^2 + 2*θᵢ[1]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
+        term2 =  neg_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+        term3 =  pos_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
 
         return -(term1 + term2 + term3)
     end
@@ -236,19 +273,19 @@ for j = 1:3
     #%% Draw Figure 3 for portfolio j
     function Equation20(θᵢ,μ̂)
 
-        term1 = θᵢ[j] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[j]^2 * σᵢ^2 + 2*θᵢ[j]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
-        term2 =  neg_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
-        term3 =  pos_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+        term1 = θᵢ[1] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[1]^2 * σᵢ^2 + 2*θᵢ[1]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
+        term2 =  neg_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+        term3 =  pos_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
 
         return -(term1 + term2 + term3)
     end
     
     #θᵢ_rand = LinRange(0.00001,0.002,50)
-    θᵢ_rand = LinRange(0.00001,0.002,50)
+    θᵢ_rand = LinRange(0.0001,0.009,50)
     u_rand = Equation20.(θᵢ_rand,μ̂[j])
 
     #θᵢ_rand_neg = LinRange(-0.001,-0.00001,50)
-    θᵢ_rand_neg = LinRange(-0.001,-0.0001,50)
+    θᵢ_rand_neg = LinRange(-0.009,-0.0001,50)
     u_rand_neg = Equation20.(θᵢ_rand_neg,μ̂[j])
 
     θᵢ_rand_all = [θᵢ_rand_neg; θᵢ_rand]
@@ -263,66 +300,65 @@ for j = 1:3
     xlabel!("θ₁", xguidefontsize=10)
     ylabel!("utility", yguidefontsize=10)
     title!("Objective function of Equation 20", titlefontsize=10)
-    savefig("Figure3_portfolio$(j).png")
+    savefig("Figure3_kat_portfolio$(j).png")
 
-    println("done with fig 3, starting on fig 4")
-
+    #println("done with fig 3, starting on fig 4")
     #%% Draw Figure 4 for portfolio j
-    function Equation20(θᵢ,μ̂)
+    # function Equation20(θᵢ,μ̂)
 
-        term1 = θᵢ[j] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[j]^2 * σᵢ^2 + 2*θᵢ[j]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
-        term2 =  neg_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
-        term3 =  pos_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+    #     term1 = θᵢ[1] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[1]^2 * σᵢ^2 + 2*θᵢ[1]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
+    #     term2 =  neg_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+    #     term3 =  pos_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
 
-        return -(term1 + term2 + term3)
-    end
+    #     return -(term1 + term2 + term3)
+    # end
 
-    function Equation20_MV(θᵢ,μ̂)
+    # function Equation20_MV(θᵢ,μ̂)
 
-        term1 = θᵢ[j] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[j]^2 * σᵢ^2 + 2*θᵢ[j]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
-        # term2 =  neg_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
-        # term3 =  pos_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+    #     term1 = θᵢ[1] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[1]^2 * σᵢ^2 + 2*θᵢ[1]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
+    #     # term2 =  neg_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+    #     # term3 =  pos_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
 
-        return -(term1)
-    end
+    #     return -(term1)
+    # end
 
-    function Equation20_PT(θᵢ,μ̂)
+    # function Equation20_PT(θᵢ,μ̂)
 
-        # term1 = θᵢ[j] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[j]^2 * σᵢ^2 + 2*θᵢ[j]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
-        term2 =  neg_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
-        term3 =  pos_integral20(θᵢ[j], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+    #     # term1 = θᵢ[1] * (μ̂ + (nu * xi)/(nu-2) - Rf) - γ̂ / 2 *(θᵢ[1]^2 * σᵢ^2 + 2*θᵢ[1]*(βᵢ*σm^2 - theta_mi * σᵢ^2))
+    #     term2 =  neg_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
+    #     term3 =  pos_integral20(θᵢ[1], μ̂, Si, xi, g_i,theta_i_minus1,lamb, b0)
 
-        return -(term2 + term3)
-    end
-    hetro_mu = 0.5405 #CHANGE - between 0.539 (second utility is too low) and 0.515 (too high)
+    #     return -(term2 + term3)
+    # end
+    # hetro_mu = 0.5405 #CHANGE - between 0.539 (second utility is too low) and 0.515 (too high)
 
-    θᵢ_rand = LinRange(0.00001,0.25,100)
-    u_rand = Equation20.(θᵢ_rand,hetro_mu)
-    MV_rand = Equation20_MV.(θᵢ_rand,hetro_mu)
-    PT_rand = Equation20_PT.(θᵢ_rand,hetro_mu)
+    # θᵢ_rand = LinRange(0.0001,0.25,100)
+    # u_rand = Equation20.(θᵢ_rand,hetro_mu)
+    # MV_rand = Equation20_MV.(θᵢ_rand,hetro_mu)
+    # PT_rand = Equation20_PT.(θᵢ_rand,hetro_mu)
 
-    θᵢ_rand_neg = LinRange(-0.01,-0.0001,50)
-    u_rand_neg = Equation20.(θᵢ_rand_neg,hetro_mu)
-    MV_rand_neg = Equation20_MV.(θᵢ_rand_neg,hetro_mu)
-    PT_rand_neg = Equation20_PT.(θᵢ_rand_neg,hetro_mu)
+    # θᵢ_rand_neg = LinRange(-0.01,-0.0001,50)
+    # u_rand_neg = Equation20.(θᵢ_rand_neg,hetro_mu)
+    # MV_rand_neg = Equation20_MV.(θᵢ_rand_neg,hetro_mu)
+    # PT_rand_neg = Equation20_PT.(θᵢ_rand_neg,hetro_mu)
 
-    θᵢ_rand_all = [θᵢ_rand_neg; θᵢ_rand]
-    u_rand_all = [u_rand_neg; u_rand]
-    MV_rand_all = [MV_rand_neg; MV_rand]
-    PT_rand_all = [PT_rand_neg; PT_rand]
+    # θᵢ_rand_all = [θᵢ_rand_neg; θᵢ_rand]
+    # u_rand_all = [u_rand_neg; u_rand]
+    # MV_rand_all = [MV_rand_neg; MV_rand]
+    # PT_rand_all = [PT_rand_neg; PT_rand]
 
-    #   Plot graphs
-    # gr()
-    # Plots.GRBackend()
-    pyplot()
-    Plots.PyPlotBackend()
-    plot(θᵢ_rand_all, -u_rand_all, w=2,xlims=(-0.01,0.25), ylims=(-0.004,0.004) ,color=:red, leg = false, dpi=300)
-    plot!(θᵢ_rand_all, -MV_rand_all, linestyle=:dash, w=1,xlims=(-0.01,0.25), ylims=(-0.004,0.004) ,leg = false, dpi=300)
-    plot!(θᵢ_rand_all, -PT_rand_all, linestyle=:dashdot, w=1,xlims=(-0.01,0.25), ylims=(-0.004,0.004) ,leg = false, dpi=300)
-    xlabel!("θ₁", xguidefontsize=10)
-    ylabel!("utility", yguidefontsize=10)
-    title!("Objective function for Decile 1", titlefontsize=10)
-    savefig("Figure4_kat.png")
+    # #   Plot graphs
+    # # gr()
+    # # Plots.GRBackend()
+    # pyplot()
+    # Plots.PyPlotBackend()
+    # plot(θᵢ_rand_all, -u_rand_all, w=2,xlims=(-0.01,0.25), ylims=(-0.004,0.004) ,color=:red, leg = false, dpi=300)
+    # plot!(θᵢ_rand_all, -MV_rand_all, linestyle=:dash, w=1,xlims=(-0.01,0.25), ylims=(-0.004,0.004) ,leg = false, dpi=300)
+    # plot!(θᵢ_rand_all, -PT_rand_all, linestyle=:dashdot, w=1,xlims=(-0.01,0.25), ylims=(-0.004,0.004) ,leg = false, dpi=300)
+    # xlabel!("θ₁", xguidefontsize=10)
+    # ylabel!("utility", yguidefontsize=10)
+    # title!("Objective function for Decile 1", titlefontsize=10)
+    # savefig("Figure4_kat_portfolio$(j).png")
 
 end
 println(μ̂,)
